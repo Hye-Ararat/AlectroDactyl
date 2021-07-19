4a13aa54feae71ddaa0c174ba444c7593adb64c9
Wolfo-Gaming <klokko06@gmail.com>
2021-07-19 19:51:53 +0200

    Oh wow i destroyed all my code
    What am i gonan do @#!#!@#!@#!@#!@#!@#!@#!@31231245141234124124

--- src/api/coins/getCoins.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import Cookies from 'js-cookie'
-async function getCoins(callback){
-    var coin_data = await axios.get(`https://clientbackend.enzonet.nl/coins?token=${Cookies.get('token')}`)
-    callback(coin_data.data.toString())
-}
-export default getCoins
--- src/api/resources/getMaxCpu.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import Cookies from 'js-cookie'
-async function getMaxCpu(callback){
-    var max_cpu_data = await axios.get(`https://clientbackend.enzonet.nl/max_cpu?token=${Cookies.get('token')}`)
-    callback(max_cpu_data.data)
-}
-export default getMaxCpu
